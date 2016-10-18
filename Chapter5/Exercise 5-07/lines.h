45018216aa5cfae3fb83acd9a141c3ca743eb595
Dren Kajmakci <dk18184@gmail.com>
2016-10-19 01:14:23 +0200

    task: _ is reserved for file maniulation

--- Chapter5/Exercise 5-07/lines.h
+++ Chapter5/Exercise 5-07/lines.h
@@ -7,6 +7,6 @@
 #define MAXLINES 50 /* max #lines to be sorted */
 #define MAXLEN 100 /* max length of any input line */
 
-int _getline(char *, int);
+int xgetline(char *, int);
 int readlines(char *lineptr[], int nlines);
 void writelines(char *lineptr[], int nlines);
