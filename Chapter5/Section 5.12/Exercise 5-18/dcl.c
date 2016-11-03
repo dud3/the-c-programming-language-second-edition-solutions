753514df0a97fb12d9e68ba6f5133ae915d888ef
textbox_test <dren@drooids.com>
2016-11-03 12:20:34 +0000

    wip: exercise 5-18

--- Chapter5/Section 5.12/Exercise 5-18/dcl.c
+++ Chapter5/Section 5.12/Exercise 5-18/dcl.c
@@ -5,7 +5,7 @@
 #define MAXTOKEN 100
 enum { NAME, PARENS, BRACKETS };
 
-int tokentype; /*type of last token  ALSO [4] !!! */
+int tokentype;
 char token[MAXTOKEN]; /*last token string */
 char name[MAXTOKEN]; /*identifier name */
 char datatype[MAXTOKEN]; /*data type = char, int, etc. */
@@ -23,21 +23,41 @@ Grammar:
 					(dcl)
 					direct-dcl()
 					direct-dcl[optional size]
+
+E.x.:
+	Exp: int a
+	a -> dcl
+	dcl -> dir-dcl
+	dir->dcl -> name
+
+	Exp: int (*arr[])
+	(*arr[]) -> dcl
+	dcl -> dir-dcl -> (...)
+	dir-dcl -> []
+	dir-dcl -> *
+	dir-dcl -> arr
+	dir-dcl -> name
 */
 
 int main() /* convert declaration to words */
 {
     while (gettoken() != EOF) {    /* 1st token on line */
+		while (tokentype != '\n') {
 
-        strcpy(datatype, token);   /* is the datatype */
-        out[0] = '\0';
+			/* 1. gettoken() gets the datatype from the token  */
+	        strcpy(datatype, token);
 
-        dcl();            /* parse rest of line */
+	        /* 2. Init out to end of the line? */
+	        out[0] = '\0';
 
-        if (tokentype != '\n')
-            printf("syntax error\n");
+			 /* parse rest of line */
+	        dcl();
 
-        printf("%s: %s %s\n", name, out, datatype);
+	    	if (tokentype != '\n')
+	    		printf("syntax error\n");
+
+	       printf("%s: %s %s\n", name, out, datatype);
+		}
     }
 
     return 0;
@@ -45,20 +65,28 @@ int main() /* convert declaration to words */
 
 int gettoken(void) /* return next token */
 {
+	int i;
     int c, getch(void);
     void ungetch(int);
-    char *p = token;        /* [1] */
+    char *p = token;
 
 	/* Skip blank spaces and tabs */
     while ((c = getch()) == ' ' || c == '\t')
     	;
 
+	/* Assume anything past one of these characters is something
+	 * we do not care to process. */
+	if (c == ';' || c == '{' || c == '=') {
+		while (c != '\n')
+			c = getch();
+	}
+
     if (c == '(') {
 
         if ((c = getch()) == ')') {
 
-            strcpy(token, "()"); /* [2][3] */
-            return tokentype = PARENS; /* [4] */
+            strcpy(token, "()");
+            return tokentype = PARENS;
 
         } else {
             ungetch(c);
@@ -67,20 +95,32 @@ int gettoken(void) /* return next token */
 
     } else if (c == '[') {
 
-        for (*p++ = c; (*p++ = getch()) != ']'; )
-        	;
+        for (*p++ = c, i = 0; (c = getch()); ++i) {
+        	if (isdigit(c)) {
+				*p++ = c;
+        	} else if (c == ']') {
+				*p++ = c;
+				break;
+			} else {
+				printf("error: missing ]\n");
+				*p++ = ']';
+				ungetch(c);
+				break;
+			}
+        }
 
         *p = '\0';
         return tokentype = BRACKETS;
 
-    } else if (isalpha(c)) {
+    } else if (isalpha(c) || c == '_') {
 
 		/* Reads the next character of input */
-        for (*p++ = c; isalnum(c = getch()); ) /* SUPPOSING [5] */
+        for (*p++ = c; isalnum(c = getch()) || c == '_';) {
             *p++ = c;
+        }
 
         *p = '\0';
-        ungetch(c); /* Get back the space or tab */
+        ungetch(c); /* Get back the space, tab */
 
         return tokentype = NAME;
 
@@ -107,14 +147,20 @@ void dirdcl(void)
 {
     int type;
 
+	if (tokentype == '\n') {
+		return;
+	}
+
     if (tokentype == '(') {
 
         dcl();
+
         if (tokentype != ')')
             printf("error: missing )\n");
-
-    } else if (tokentype == NAME) /* variable name */
+    }
+    else if (tokentype == NAME) { /* variable name */
         strcpy(name, token);
+    }
     else
         printf("error: expected name or (dcl)\n");
 
